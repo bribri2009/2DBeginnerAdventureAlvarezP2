a007cd6c1be1a031cd3e4af5f673f6b54e05a514
bribri2009 <735857@student.dusd.net>
2024-10-30 09:59:54 -0700

    worked on 2d beginner project
    made character move added rigidbody and added prefabs

--- 2DBeginnerAdventureAlvarezP2/Assets/Scripts/MarshmellowConsole.cs
+++ 2DBeginnerAdventureAlvarezP2/Assets/Scripts/MarshmellowConsole.cs
@@ -1,26 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class MarshMellowConsole : MonoBehaviour
 {
-    public float speed = 4.0f;
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        
-        Vector2 position = transform.position;
-        position.x = position.x + speed * horizontal * Time.deltaTime;
-        position.y = position.y + speed * vertical * Time.deltaTime;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+    }
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime; ;
 
         transform.position = position;
     }
